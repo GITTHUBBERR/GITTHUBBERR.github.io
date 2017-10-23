4400bce5289acf66b3a2002f41bb8852dbf4f9df
GITTHUBBERR <31257202+GITTHUBBERR@users.noreply.github.com>
2017-10-23 00:23:45 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,397 @@
+var data = [
+    {
+    id: 1,
+    company: 'Company 01',
+    contact01: 'Ted Smith',
+    contact02: 'Sandra Chu',
+    email01: 'ted.smith@company.com',
+    email02: 'sandra.chu@company.com',
+    firstcontact: '2016.02.05',
+    reply: true,
+    status: 'Researching',
+    '2016netprofit': '1,909,332'
+},
+    {
+        id: 2,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: false,
+        status: 'Approved',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 3,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Researching',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 4,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: false,
+        status: 'Pending',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 5,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Pending',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 6,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: false,
+        status: 'Declined',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 7,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: false,
+        status: 'Researching',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 8,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: false,
+        status: 'Pending',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 9,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Researching',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 10,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Researching',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 11,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: false,
+        status: 'Pending',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 12,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Approved',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 13,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Pending',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 14,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Declined',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 15,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: false,
+        status: 'Researching',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 16,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: false,
+        status: 'Pending',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 17,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Researching',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 18,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Researching',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 19,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: false,
+        status: 'Pending',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 20,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Pending',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 21,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Researching',
+        '2016netprofit': '1,909,332'
+    },
+        {
+        id: 22,
+        company: 'Company 01',
+        contact01: 'Ted Smith',
+        contact02: 'Sandra Chu',
+        email01: 'ted.smith@company.com',
+        email02: 'sandra.chu@company.com',
+        firstcontact: '2016.02.05',
+        reply: true,
+        status: 'Pending',
+        '2016netprofit': '1,909,332'
+    },
+    {
+        id: 23,
+        company: 'Company 23',
+        contact01: 'Fred Smith',
+        contact02: 'Samantha Chu',
+        email01: 'fred.smith@company.com',
+        email02: 'samantha.chu@company.com',
+        firstcontact: '2017.01.02',
+        reply: false,
+        status: 'Approved',
+        '2016netprofit': '2,339,332'
+    }
+];
+
+var app = angular.module('myApp', ['fancygrid-angularjs']);
+
+app.controller('MainController', function ($scope, $interval, $compile) {
+    $scope.config = {
+        theme: 'bootstrap',
+        title: 'Acquisitions Tool',
+        renderTo: 'container',
+        width: 1018,
+        height: 470,
+        data: data,
+        paging: true,
+        selModel: 'rows',
+        trackOver: true,
+        tbar: [{
+            type: 'button',
+            text: 'Add',
+            action: 'add'
+        }, {
+            type: 'button',
+            text: 'Delete',
+            action: 'remove'
+            }, {
+                type: 'button',
+                text: 'Save',
+                action: 'save'
+            }],
+        defaults: {
+            type: 'string',
+            width: 100,
+            editable: true,
+            sortable: true,
+            ellipsis: true,
+            resizable: false
+        },
+        clicksToEdit: 1,
+        columns: [{
+            index: 'id',
+            title: 'ID',
+            type: 'number',
+            editable: false,
+            width: 35
+        },
+            {
+            index: 'company',
+            title: 'Company',
+            width: 90
+        },
+            {
+            index: 'contact01',
+            title: 'Contact 01',
+            width: 90
+            },
+            {
+                index: 'email01',
+                title: 'Email 01',
+                vtype: 'email',
+                width: 150
+            },
+            {
+            index: 'contact02',
+            title: 'Contact 02',
+            width: 90
+         },
+            {
+            index: 'email02',
+            title: 'Email 02',
+            vtype: 'email',
+            width: 150    
+        },
+            {
+            index: 'firstcontact',
+            title: '1st Contact',
+            type: 'date',
+            width: 90,
+            format: {
+                read: 'Y.m.d',
+                write: 'm/d/Y',
+                edit: 'm/d/Y'
+            }
+        },
+            {
+            index: 'reply',
+            title: 'Reply',
+            type: 'checkbox',
+            width: 55
+        },
+            {
+            index: 'status',
+            title: 'Status',
+            type: 'combo',
+            data: ['Researching', 'Pending', 'Approved', 'Declined']
+        },
+            {
+                index: '2016netprofit',
+                title: '2016 Profits',
+                type: 'string'
+        },
+             {
+            type: 'action',
+            width: 65,
+            items: [{
+                text: 'Delete',
+                cls: 'action-column-remove',
+                action: 'remove'
+            }],
+        }]
+
+    };
+});
+
+
+
+
+
+
+
+
+
